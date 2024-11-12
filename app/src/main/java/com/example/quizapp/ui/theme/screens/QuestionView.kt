f1093ec54c2b85e7aec4bf20bfb6cc73b490e58f
victor-eidt <victoreidtrl@gmail.com>
2024-11-12 19:47:26 -0300

    adição do som e do sistema de pontos

--- app/src/main/java/com/example/quizapp/ui/theme/screens/QuestionView.kt
+++ app/src/main/java/com/example/quizapp/ui/theme/screens/QuestionView.kt
@@ -1,5 +1,8 @@
 package com.example.quizapp.ui.theme.screens
 
+import android.media.MediaPlayer
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,16 +14,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.quizapp.R
 import com.example.quizapp.data.Question
+import com.example.quizapp.data.Score
+import com.example.quizapp.R
 import com.example.quizapp.ui.theme.poppins
 import kotlinx.coroutines.delay
-import com.example.quizapp.data.Score
 
 @Composable
 fun QuestionView(
@@ -29,107 +33,110 @@ fun QuestionView(
     onAnswerSelected: (isCorrect: Boolean) -> Unit,
     onQuizEnd: () -> Unit
 ) {
-    // Embaralhar opções de resposta e encontrar o índice correto após o embaralhamento
-    val shuffledOptions = remember(question) {
-        question.options.shuffled()
-    }
-    val correctAnswerIndex = remember(question) {
-        shuffledOptions.indexOf(question.options[question.correctAnswerIndex])
-    }
-
+    val context = LocalContext.current
     var selectedOptionIndex by remember { mutableStateOf<Int?>(null) }
-    var buttonColors by remember { mutableStateOf(List(shuffledOptions.size) { Color.White }) }
+    var buttonColors by remember { mutableStateOf(List(question.options.size) { Color.White }) }
     var isCorrectAnswer by remember { mutableStateOf(false) }
     var showNextQuestion by remember { mutableStateOf(false) }
 
     if (showNextQuestion) {
         selectedOptionIndex = null
-        buttonColors = List(shuffledOptions.size) { Color.White }
+        buttonColors = List(question.options.size) { Color.White }
         showNextQuestion = false
     }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.background_image),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxSize()
-        )
-
-        Column(
+    Crossfade(
+        targetState = question,
+        animationSpec = tween(durationMillis = 600)
+    ) { currentQuestion ->
+        Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
         ) {
-            Text(
-                text = question.text,
-                fontSize = 20.sp,
-                color = Color.White,
-                fontFamily = poppins,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(bottom = 32.dp),
-                textAlign = TextAlign.Center
+            Image(
+                painter = painterResource(id = R.drawable.background_image),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
             )
 
-            question.imageResId?.let {
-                Image(
-                    painter = painterResource(id = it),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(150.dp)
-                        .padding(bottom = 16.dp),
-                    contentScale = ContentScale.Fit
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = currentQuestion.text,
+                    fontSize = 20.sp,
+                    color = Color.White,
+                    fontFamily = poppins,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 32.dp),
+                    textAlign = TextAlign.Center
                 )
-            }
 
-            shuffledOptions.forEachIndexed { index, option ->
-                Button(
-                    onClick = {
-                        if (selectedOptionIndex == null) {
-                            selectedOptionIndex = index
-                            isCorrectAnswer = index == correctAnswerIndex
-                            buttonColors = buttonColors.toMutableList().apply {
-                                this[index] = if (isCorrectAnswer) Color.Green else Color.Red
-                            }
-                        }
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 8.dp),
-                    shape = RoundedCornerShape(50),
-                    colors = ButtonDefaults.buttonColors(
-                        buttonColors[index]
-                    )
-                ) {
-                    Text(
-                        text = option,
-                        color = Color.Black,
-                        fontSize = 18.sp,
-                        fontFamily = poppins
+                currentQuestion.imageResId?.let {
+                    Image(
+                        painter = painterResource(id = it),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(150.dp)
+                            .padding(bottom = 16.dp),
+                        contentScale = ContentScale.Fit
                     )
                 }
+
+                currentQuestion.options.forEachIndexed { index, option ->
+                    Button(
+                        onClick = {
+                            if (selectedOptionIndex == null) {
+                                selectedOptionIndex = index
+                                isCorrectAnswer = index == currentQuestion.correctAnswerIndex
+                                buttonColors = buttonColors.toMutableList().apply {
+                                    this[index] = if (isCorrectAnswer) Color.Green else Color.Red
+                                }
+
+                                if (isCorrectAnswer) {
+                                    val mediaPlayer = MediaPlayer.create(context, R.raw.correct_sound)
+                                    mediaPlayer.start()
+                                    mediaPlayer.setOnCompletionListener { it.release() }
+                                }
+                            }
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp),
+                        shape = RoundedCornerShape(50),
+                        colors = ButtonDefaults.buttonColors(
+                            buttonColors[index]
+                        )
+                    ) {
+                        Text(
+                            text = option,
+                            color = Color.Black,
+                            fontSize = 18.sp,
+                            fontFamily = poppins
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                Text(
+                    text = "Score: ${score.totalScore}",
+                    fontSize = 18.sp,
+                    color = Color.White,
+                    fontFamily = poppins,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(top = 16.dp)
+                )
             }
-
-            Spacer(modifier = Modifier.height(24.dp))
-
-            // Exibir pontuação na parte inferior da tela
-            Text(
-                text = "Score: ${score.totalScore}",
-                fontSize = 18.sp,
-                color = Color.White,
-                fontFamily = poppins,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.padding(top = 16.dp)
-            )
         }
     }
-
+    
     LaunchedEffect(selectedOptionIndex) {
         if (selectedOptionIndex != null) {
             delay(1000)
